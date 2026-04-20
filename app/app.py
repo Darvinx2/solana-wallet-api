233ae09a2a1459b2e23b3ae20ace9c29f004575f
s0lmi <64921902+Darvinx2@users.noreply.github.com>
2026-04-20 21:29:31 +0300

    Add portfolio endpoint

--- app/app.py
+++ app/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 
+from api.portfolio import bp as bp_get_portfolio
+from app.container import build_services
 from app.extensions import db, migrate, settings
+from services.wallet_portfolio_service import WalletPortfolioService
 from utils.logger import setup_logger
 
 
-def create_app() -> Flask:
-    app = Flask(__name__)
+class App(Flask):
+    portfolio_service: WalletPortfolioService
+
+
+def create_app() -> App:
+    app = App(__name__)
 
     app.config.from_object(settings)
 
@@ -13,5 +20,10 @@ def create_app() -> Flask:
     migrate.init_app(app, db)
 
     setup_logger(app)
+
+    services = build_services()
+    app.portfolio_service = services["portfolio_service"]
+
+    app.register_blueprint(bp_get_portfolio)
 
     return app
