87a6222830db2a99a02a17b711b7121d1a013e88
solmi <darvineng228@gmail.com>
2026-04-20 23:21:19 +0300

    feat: add farm endpoint

--- app/app.py
+++ app/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 
+from api.farm import bp as bp_farm
 from api.portfolio import bp as bp_get_portfolio
-from app.container import build_services
-from app.extensions import db, migrate, settings
+from app.extensions import db, httpx_client, migrate, settings
+from clients.jupiter import JupiterPrice
+from clients.moralis import MoralisPortfolio
+from repositories.farm_repository import FarmRepository
+from repositories.user_repository import UserRepository
+from services.farm_service import FarmService
 from services.wallet_portfolio_service import WalletPortfolioService
 from utils.logger import setup_logger
 
 
 class App(Flask):
     portfolio_service: WalletPortfolioService
+    farm_service: FarmService
 
 
 def create_app() -> App:
@@ -21,9 +27,11 @@ def create_app() -> App:
 
     setup_logger(app)
 
-    services = build_services()
-    app.portfolio_service = services["portfolio_service"]
+    portfolio_service = WalletPortfolioService(MoralisPortfolio(httpx_client), JupiterPrice(httpx_client))
+    app.portfolio_service = portfolio_service
+    app.farm_service = FarmService(FarmRepository(), UserRepository(), portfolio_service)
 
     app.register_blueprint(bp_get_portfolio)
+    app.register_blueprint(bp_farm)
 
     return app
